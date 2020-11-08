26e911726f0ca0c50f4a1e16ce9f77874f672bc8
ZZZZZ <ZZZZZ@mail.ru>
2020-11-08 11:54:54 +0300

    Новая кнопка

--- ov4_07.11.2020/script.js
+++ ov4_07.11.2020/script.js
@@ -1,10 +1,13 @@
 console.log("Hello!");
 const button = document.getElementById('button')
+const button2 = document.getElementById('button2')
 const ul = document.getElementById('ul')
+const ul2 = document.getElementById('ul2')
 console.log(button);
 
 let offset = 0;
-let limit = 4;
+let limit = 1;
+let num = 100;
 
 function delay(ms, response) {
     return new Promise((resolve, reject) => {
@@ -20,13 +23,12 @@ button.addEventListener('click', async function (event) {
         await delay(1000);
 
         button.forEach(item => {
-
            
             const newElement = document.createElement("li");
             newElement.classList.add("list-group-item")
             newElement.innerHTML = `<i class="fab fa-angellist"></i> ${item.username}: ${item.message}`;
             ul.append(newElement);
-
+            console.log(item);
             offset++;
 
         });
@@ -35,3 +37,24 @@ button.addEventListener('click', async function (event) {
 
 })
 
+
+button2.addEventListener('click', async function (event) {
+    let response = await fetch(`http://user08.test1.seschool.ru:3000/api/chat/?offset=${num}&limit=${limit}`)
+    if (response.ok) {
+        let button2 = await response.json()
+        await delay(1000);
+
+        button2.forEach(item => {
+           
+            const newElement = document.createElement("li");
+            newElement.classList.add("list-group-item")
+            newElement.innerHTML = `<i class="fab fa-angellist"></i> ${item.username}: ${item.message}`;
+            ul2.append(newElement);
+            console.log(item);
+            num--;
+
+        });
+
+    }
+
+})
